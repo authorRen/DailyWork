177934c783a7b6d90e77b023cc64e93f15d2f408
renzeqiang <renzeqiang@youyuwo.com>
2017-04-12 11:28:27 +0800

    [提交] 首次提交

---
+++ app/src/main/java/com/caiyi/dailywork/compant/MainActivity.java
@@ -0,0 +1,57 @@
+package com.caiyi.dailywork.compant;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.caiyi.dailywork.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvResolution;
+    private Button btnPress;
+    /** 获取手机屏幕分辨率相关类*/
+    private DisplayMetrics dm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        tvResolution = (TextView) findViewById(R.id.tv_resolution);
+        btnPress = (Button) findViewById(R.id.btn_press);
+        btnPress.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_press:
+                getResolution();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    /**
+     * 获取手机屏幕的分辨率
+     */
+    private void getResolution() {
+        dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        //获取手机屏幕的宽度和高度像素单位为px
+        String str = getResources().getString(R.string.resolution) + dm.widthPixels  + "*" + dm.heightPixels;
+        tvResolution.setText(str);
+    }
+}
---
+++ app/src/main/java/com/caiyi/dailywork/ui/FlowView.java
@@ -0,0 +1,130 @@
+package com.caiyi.dailywork.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RZQ on 2017/4/1.
+ */
+
+public class FlowView extends ViewGroup {
+    private static final int LEFT = -1;
+    private static final int CENTER = -1;
+    private static final int RIGHT = -1;
+
+    protected List<List<View>> mAllViews = new ArrayList<>();
+    protected List<Integer> mLineHeight = new ArrayList<>();
+    protected List<Integer> mLineWidth = new ArrayList<>();
+
+    private int mGravity;
+    private List<View> lineViews = new ArrayList<>();
+
+    public FlowView(Context context) {
+        this(context, null);
+    }
+
+    public FlowView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FlowView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mGravity = LEFT;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        //wrap_content
+        int width = 0;
+        int height = 0;
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) {
+                if (i == cCount - 1) {
+                    width = Math.max(lineWidth, width);
+                }
+                continue;
+            }
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+
+            if (lineWidth + childWidth > sizeWidth - getPaddingLeft() - getPaddingRight()) {
+                width = Math.max(width, lineWidth);
+                lineWidth = childWidth;
+                height += lineHeight;
+                lineHeight = childHeight;
+            } else {
+                lineWidth += childWidth;
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            if (i == cCount - 1) {
+                width = Math.max(lineWidth, width);
+                height += lineHeight;
+            }
+        }
+        setMeasuredDimension(
+                modeWidth == MeasureSpec.EXACTLY ? sizeWidth : width + getPaddingLeft() + getPaddingRight(),
+                modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height + getPaddingTop() + getPaddingBottom()
+        );
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mAllViews.clear();
+        mLineHeight.clear();
+        mLineWidth.clear();
+        lineViews.clear();
+
+        int width = getWidth();
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) continue;
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+
+            if (childWidth + lineWidth + lp.leftMargin + lp.rightMargin > width - getPaddingLeft() - getPaddingRight()) {
+                mLineHeight.add(lineHeight);
+                mAllViews.add(lineViews);
+                mLineWidth.add(lineWidth);
+
+                lineWidth = 0;
+                lineHeight = childHeight + lp.topMargin + lp.bottomMargin;
+                lineViews = new ArrayList<View>();
+            }
+            mLineHeight.add(lineHeight);
+            mLineWidth.add(lineWidth);
+            mAllViews.add(lineViews);
+
+            int left = getPaddingLeft();
+            int top = getPaddingTop();
+
+            int lineNum = mAllViews.size();
+
+        }
+    }
+}
